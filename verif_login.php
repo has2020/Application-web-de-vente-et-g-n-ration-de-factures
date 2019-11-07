26adee7a058465563661492df71c2b13b200aab8
Hassan-Soufi <48534627+has2020@users.noreply.github.com>
2019-11-07 16:46:01 +0100

    Update verif_login.php

--- verif_login.php
+++ verif_login.php
@@ -3,7 +3,7 @@
 
 try
 {
-	$bdd = new PDO('mysql:host=localhost;dbname=projet;charset=utf8', 'root', 'naroto10',array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+	$bdd = new PDO('mysql:host=localhost;dbname=projet;charset=utf8', 'root', 'root',array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 	
 }
 catch (Exception $e)
